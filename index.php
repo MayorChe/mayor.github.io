ff7d1c0b5fb328b4ef3d2abdc5195b6a8de49dd0
MayorChe <49272380+MayorChe@users.noreply.github.com>
2019-04-04 12:50:50 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<html>
+
+	<head>
+	
+		<meta charset="utf-8">
+        <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+		<link rel="stylesheet" href="css/css.css" >
+		<title>Family Stuff</title>
+		
+	</head>
+	
+	
+	<body>
+		
+		<div class="main">
+		</div>
+		
+		<div class="top">
+		
+			<div class="but1">				
+				<a class="but1a" href="index.php">	
+					<p class="but1p">Доброе Утро</p>					
+				</a>
+			</div>
+			
+			<div class="but2">	
+				<a class="but2a" href="index2.php">	
+					<p class="but2p">Загляни Сюда</p>	
+				</a>	
+			</div>
+		
+		</div>
+		
+		<div class="stuff">
+		
+				<div class="first">
+					<p class="first1">Доброе утро Любимая! <3</p>
+					<p class="first2">Приятного аппетита Моя Маленькая!</p>
+					<p class="first3">Здесь будут все необходимые тебе ссылки <3</p>
+				</div>
+				
+				<div class="second">
+					<a class="butvk" href="https://vk.com/milaxa_krsk" target="_blank"><img class="shad" src="images/vk.png" width="100px" height="100px"></a>
+					<a class="buttw" href="https://www.twitch.tv/directory?tl=1" target="_blank"><img src="images/twitch.png" width="100px" height="100px"></a>
+					<a class="butyou" href="https://www.youtube.com/" target="_blank"><img src="images/you.png" width="100px" height="100px"></a>
+					<a class="butma" href="https://mail.ru/" target="_blank"><img src="images/mail.png" width="100px" height="100px"></a>
+					<p class="seccont">Новый День - Новая Жизнь!<br>Доброе Утро Солнце!<br>Пусть этот день будет самым прекрасным!<br>Как и все последующие дни жизни!<br>Люблю тебя Моя Девочка <3!!!<br>Очень соскучился и жду конца дня!<br><br>С Любовью, Твой Пирожочек <3 </p>
+				</div>
+				
+				<div class="foot">
+					<p class="foot1">Люблю Тебя Моя Маленькая!</p>
+				</div>
+		</div>
+		
+	</body>
+	
+	
+</html>
